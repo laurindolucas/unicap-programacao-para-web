d7e11ac1c3f20af1e4d5a6d98c7ecfd27b2dc970
Lucas Laurindo <caiolucax2024@gmail.com>
2025-09-09 22:54:32 +0000

    projeto agenda

---
+++ agenda.js
@@ -0,0 +1,47 @@
+console.log(
+    " ======== MENU ======== \n",
+    "1 - LISTAR TODOS OS CONTATOS \n",
+    "2 - ADICIONAR NOVO CONTATO \n",
+    "3 - EDITAR CONTATO \n",
+    "4 - REMOVER CONTATO"
+);
+
+const escolha = prompt("Digite o número equivalente ao que deseja fazer");
+
+
+function listarContato(){
+
+};
+
+function adicionarContato(){
+
+};
+
+function editarContato(){
+
+};
+
+function removerContato(){
+
+};
+
+
+switch(escolha){
+    case 1 :
+        listarContato();
+        break;
+    case 2:
+        adicionarContato();
+        break;
+    case 3:
+        editarContato();
+        break;
+    case 4: 
+        removerContato();
+        break;
+}
+
+
+
+
+
